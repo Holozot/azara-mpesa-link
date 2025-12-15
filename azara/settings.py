2230f77c3a17923724160c8359e3fce223eff09d
Holozot <kelwgra17@gmail.com>
2025-12-15 11:56:34 +0300

    correct debug

--- azara/settings.py
+++ azara/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-@+qme4ygn2u0%7aa-8n)4
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = 'RENDER' not in os.environ
-DEBUG = True
+DEBUG = False
 
 # Allow ALL hosts to prevent DisallowedHost errors
 ALLOWED_HOSTS = ['*']
